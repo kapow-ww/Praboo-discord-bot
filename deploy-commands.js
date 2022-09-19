696af6bf7d9e9396bb2dfc0161546812f3a8b9a8
Broccolidiff <takurokung55@gmail.com>
2022-09-19 19:07:19 +0700

    New Alert

--- deploy-commands.js
+++ deploy-commands.js
@@ -16,15 +16,29 @@ for (const file of commandFiles) {
   const filePath = path.join(commandsPath, file);
   const command = require(filePath);
   commands.push(command.data.toJSON());
-  console.log(command.data);
+  console.log(command.data.name);
 }
 
 const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
 //create commands
-rest
-  .put(Routes.applicationCommands(process.env.CLIENT_ID), {
-    body: commands,
-  })
-  .then(() => console.log("Successfully registered application commands."))
-  .catch(console.error);
+(async () => {
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`
+    );
+
+    const data = await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      {
+        body: commands,
+      }
+    );
+
+    console.log(
+      `Successfully reloaded ${data.length} application (/) commands.`
+    );
+  } catch (error) {
+    console.error(error);
+  }
+})();
